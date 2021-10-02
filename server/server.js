e305de3593beeb30e9249a4a6c8b18c7219f317a
phaydara <phaydara@gmail.com>
2021-10-02 11:25:53 -0500

    REFRESHER notes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const axios = require('axios');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -9,7 +11,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
+// REVIEW: how we use a 3rd party API to request data
+// NOTES: our client makes request to /giphy with possible search parameters
+app.get('/giphy', (req, res) => {
+    // NOTES: our server makes a request to giphy API
+    // NOTES: &limit=25 is param for receiving 25 giphy images with all the data
+    axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=2&rating=g`)
+    .then(response => {
+        // NOTES: server gets response back which we forward back to client
+        res.send(response.data)
+    }).catch(error => {
+        res.sendStatus(500);
+    })
+});
 
+/*
+Client side code:
+axios.get('/giphy')
+    .then(response => {
+        // ex: giphy sends back response back as .data, so response could be response.data.data.
+        res.send(response.data); 
+            esponse.data.data.map(gif => {
+                gif.small_image.url...
+            }
+        });
+    }).catch(error => {
+        res.sendStatus(500);
+    });
+*/
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,36 @@
 
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App () {
+  const [results, setResults] = useState([]);
+
+  // REVIEW: getting data from giphy API
+  const fetchGiphyResults = () => {
+    axios({
+      method: 'GET',
+      url: '/giphy'
+    }).then(response => {
+      // response we get back is an object
+      // console.log(response);
+      // REVIEW: response is an array of objects
+      // console.log(response.data);
+      // NOTES: sift thru to get to what you really WANT
+      // NOTES: ...which is an array of GIPHY objects w/properties
+      console.log(response.data.data);
+      // REVIEW: sets resonse to variable `results`
+      setResults(response.data.data)
+
+    }).catch(error => {
+      console.log('error in fetchGiphyResults', error);
+    });
+  }
+
+  useEffect(() => {
+    fetchGiphyResults();
+  }, []);
+
 
     return (
       <div className="App">
@@ -10,6 +38,13 @@ function App () {
           <h1 className="App-title">APIS</h1>
           <h4><i>APIS</i></h4>
         </header>
+        {/* REVIEW: displays all the results as set from above */}
+        {/* {JSON.stringify(results)} */}
+        {
+          results.map(gif => (
+            <p><img src={gif.images.downsized_medium.url} /></p>
+          ))
+        }
         <br/>
       </div>
     );
